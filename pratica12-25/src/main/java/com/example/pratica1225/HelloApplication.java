1873054ff6c7a91f886b757530457b148662e5e9
DORTENZIO.21125 <dortenzio.diego.studente@itispaleocapa.it>
2025-03-24 13:57:27 +0100

    Second  Second fxml part

--- pratica12-25/src/main/java/com/example/pratica1225/HelloApplication.java
+++ pratica12-25/src/main/java/com/example/pratica1225/HelloApplication.java
@@ -5,14 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        HelloController controller=fxmlLoader.getController();
+        controller.setGestoreCsv(new GestoreCsv(new File("pratica12-25/src/main/resources/com/example/pratica1225/dati/dortenzio.csv")));
+
+        Scene scene = new Scene(fxmlLoader.load(), 900, 570);
+        stage.setTitle("GESTORE CSV");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- pratica12-25/src/main/java/com/example/pratica1225/HelloController.java
+++ pratica12-25/src/main/java/com/example/pratica1225/HelloController.java
@@ -1,14 +1,63 @@
 package com.example.pratica1225;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.animation.*;
+import javafx.fxml.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.util.*;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+public class HelloController{
+    private GestoreCsv gestoreCsv;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane box1,box2,box3;
+    @FXML
+    private Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11;
+
+    public void setGestoreCsv(GestoreCsv gestoreCsv) {
+        this.gestoreCsv = gestoreCsv;
+    }
+
+    @FXML
+    public void initialize() {
+        entrataAnchor(box1,-300,0);
+        entrataAnchor(box2,-300,0);
+        entrataAnchor(box3,1300,900);
+        b1.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b2.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b2.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b3.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b3.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b4.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b4.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseMoved(event -> b1.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b1.setOnMouseExited(event -> b1.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b10.setOnMouseMoved(event -> b10.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b10.setOnMouseExited(event -> b10.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b11.setOnMouseMoved(event -> b11.setStyle("-fx-border-color: #42f5a7; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+        b11.setOnMouseExited(event -> b11.setStyle("-fx-border-color: #bd8e60; -fx-background-color: e6ccb2; -fx-border-radius: 13 0; -fx-background-radius: 12 0;"));
+
+    }
+
+    @FXML
+    private void entrataAnchor(AnchorPane anchorPane, int xIn, int xFin) {
+        anchorPane.setTranslateX(xIn);
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1), anchorPane);
+        translateTransition.setFromX(xIn);
+        translateTransition.setToX(xFin);
+        translateTransition.setCycleCount(1);
+        translateTransition.setAutoReverse(false);
+        translateTransition.play();
     }
 }
